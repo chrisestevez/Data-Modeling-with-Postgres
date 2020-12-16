1aad64a45016839a0a503c69a04291810be6a7fe
chrisestevez <chrisestevez1@gmail.com>
2020-12-16 07:18:19 -0500

    feat: updated project
    - Finished create_tables.py
    - Finished etl.py
    - Finished sql_queries.py
    - Created ERD

--- create_tables.py
+++ create_tables.py
@@ -1,3 +1,4 @@
+
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
--- sql_queries.py
+++ sql_queries.py
@@ -11,11 +11,11 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
 songplay_id serial PRIMARY KEY,
-start_time TIMESTAMP,
-user_id INTEGER,
+start_time BIGINT REFERENCES time(start_time),
+user_id INTEGER REFERENCES users(user_id),
 level VARCHAR,
-song_id VARCHAR,
-artist_id VARCHAR,
+song_id VARCHAR REFERENCES songs(song_id),
+artist_id VARCHAR REFERENCES artists(artist_id),
 session_id INTEGER,
 location VARCHAR,
 user_agent VARCHAR
@@ -36,7 +36,7 @@ song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
 song_id VARCHAR PRIMARY KEY,
 title  VARCHAR,
-artist_id VARCHAR,
+artist_id VARCHAR NOT NULL,
 year SMALLINT,
 duration NUMERIC
 );                    
@@ -45,7 +45,7 @@ duration NUMERIC
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
 artist_id VARCHAR PRIMARY KEY,
-name VARCHAR,
+name VARCHAR NOT NULL,
 location VARCHAR,
 latitude NUMERIC,
 longitude NUMERIC
@@ -54,7 +54,7 @@ longitude NUMERIC
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-start_time TIMESTAMP PRIMARY KEY,
+start_time BIGINT PRIMARY KEY,
 hour SMALLINT,
 day SMALLINT,
 week SMALLINT,
@@ -67,27 +67,53 @@ weekday SMALLINT
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id\
+    , session_id, location, user_agent) 
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s)                        
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE
+SET level = EXCLUDED.level;
+                   
 """)
 
 song_table_insert = ("""
+INSERT INTO songs(
+song_id, title, artist_id, year, duration)
+VALUES (%s,%s,%s,%s,%s) ON CONFLICT (song_id) DO NOTHING                     
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists(
+artist_id, name, location, latitude, longitude) 
+VALUES (%s,%s,%s,%s,%s) ON CONFLICT (artist_id) DO NOTHING                      
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES(%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time)
+    DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT songs.song_id, artists.artist_id
+FROM songs
+JOIN artists ON artists.artist_id = songs.artist_id
+WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create\
+    , artist_table_create, time_table_create, songplay_table_create]
+
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop\
+    , artist_table_drop, time_table_drop]
